0ca98b3d4881badeb8a346d02931dbb971236ec1
Anmolnoor <anmolnoor59@gmail.com>
2022-05-22 03:45:13 +0530

    Solana-pay store

--- pages/api/fetchProducts.js
+++ pages/api/fetchProducts.js
@@ -1,16 +1,34 @@
+// import products from "./products.json";
+
+// export default function handler(req, res) {
+// 	// If get request
+// 	if (req.method === "GET") {
+// 		// Remove hashes from products.json
+// 		const cleanProducts = products.map((item) => {
+// 			delete item.hash;
+// 			return item;
+// 		});
+
+// 		res.status(200).json(cleanProducts);
+// 	} else {
+// 		res.status(405).send(`Method ${req.method} not allowed`);
+// 	}
+// }
+
+
 import products from "./products.json";
 
 export default function handler(req, res) {
-	// If get request
-	if (req.method === "GET") {
-		// Remove hashes from products.json
-		const cleanProducts = products.map((item) => {
-			delete item.hash;
-			return item;
-		});
+  // If get request
+  if (req.method === "GET") {
+    // Create a copy of products without the hashes and filenames
+    const productsNoHashes = products.map((product) => {
+      const { hash, filename, ...rest } = product;
+      return rest;
+    });
 
-		res.status(200).json(cleanProducts);
-	} else {
-		res.status(405).send(`Method ${req.method} not allowed`);
-	}
+    res.status(200).json(productsNoHashes);
+  } else {
+    res.status(405).send(`Method ${req.method} not allowed`);
+  }
 }
